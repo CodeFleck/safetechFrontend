f6acf0a6b67dae3cedf7c8406fbd79caeeba7d02
Daniel Fleck <danielfleck@live.ca>
2020-06-27 18:30:32 -0300

    Create delete funcionality and view user details

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -19,18 +19,30 @@ export class UsersComponent implements OnInit {
     private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.refreshData();
+  }
+
+  refreshData(){
     this.httpClientService.getUsers().subscribe(
       response => this.handleSuccessfulResponse(response),
     );
     this.activatedRoute.queryParams.subscribe(
       (params) => {
-        this.action = params['action']
+        this.action = params['action'];
+        const selectedUserId = params['id'];
+        if (selectedUserId) {
+          this.selectedUser = this.users.find(user => user.id === +selectedUserId);
+        }
       }
     );
   }
 
   handleSuccessfulResponse(response) {
     this.users = response;
+  }
+
+  viewUser(id: number) {
+    this.router.navigate(['admin','users'], {queryParams : {id, action: 'view'}});
   }
 
   addUser() {
